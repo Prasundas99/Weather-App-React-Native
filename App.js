975de1cb28cf1610af7415f2bcfc34b3f8ed502b
Prasundas99 <prasundas115@gmail.com>
2021-10-29 15:57:15 +0530

    UI updated

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
+//import * as firebase from 'firebase'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Landing from "./components/auth/Landing";
+import Register from "./components/auth/Register";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*const firebaseConfig = {
+  apiKey: "AIzaSyApLDzwCkRr6wdj0rcsclAa_LjrvnQuEdM",
+  authDomain: "linkin-park-tribute-page.firebaseapp.com",
+  databaseURL: "https://linkin-park-tribute-page.firebaseio.com",
+  projectId: "linkin-park-tribute-page",
+  storageBucket: "linkin-park-tribute-page.appspot.com",
+  messagingSenderId: "609406729652",
+  appId: "1:609406729652:web:7e9693f9965fc384cfc6e1",
+  measurementId: "G-LWV64RGCFY"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
+*/
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
